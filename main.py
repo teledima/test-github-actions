56ca5f79c5ae5d4cab921289e15b6f21207a490e
teledima <teledima00@gmail.com>
2021-06-20 00:47:52 +0500

    Try use tags

--- main.py
+++ main.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def hello_world():
     return make_response('Hello World!', 200)
+
+@app.route('/v1', methods=['GET'])
+def first_page():
+    return make_response('Release v1', )
