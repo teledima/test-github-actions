93a68147faa05993f384e33b61152e390dd12a05
teledima <teledima00@gmail.com>
2021-06-19 18:02:25 +0500

    create simple flask app

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-if __name__ == '__main__':
-    print('Hello World!')
+from flask import Flask, make_response
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return make_response('Hello World!', 200)
