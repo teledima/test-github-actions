fd055011ca4c1200c634869d51ded1dd5a82c62a
teledima <teledima00@gmail.com>
2021-06-20 17:45:55 +0500

    fix main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 def hello_world():
     return make_response('Hello World!', 200)
 
+
 @app.route('/v1', methods=['GET'])
 def first_page():
-    return make_response('Release v1', )
+    return make_response('Release v1', 200)
